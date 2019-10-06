d607ae33ac8e424ea91585a21e0675ec8f1745a7
David Dataram <davedataram@gmail.com>
2019-10-06 14:38:12 -0400

    Updated ssl location

--- proxy/startup.sh
+++ proxy/startup.sh
@@ -1,6 +1,5 @@
 # Startup the nginx server. The server has to be active for the Let's Encrypt Certbot to
 # register and install the certificates.
 nginx -g "daemon on;"
-
 # Shuts down the daemonized nginx server and fires up one in the foreground.
 nginx -s stop && nginx -g 'daemon off;'
